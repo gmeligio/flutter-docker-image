dd68347528bbba481f75f94233c79bbbc55ebdbd
Eligio Mariño <22875166+gmeligio@users.noreply.github.com>
2024-02-16 13:10:59 +0100

    ci: pass github context as variable to bash script

--- script/set_environment_variables.sh
+++ script/set_environment_variables.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-IMAGE_REPOSITORY_PATH="${{ github.repository_owner }}/${{ env.IMAGE_REPOSITORY_NAME }}"
+IMAGE_REPOSITORY_PATH="$GITHUB_REPOSITORY_OWNER/$IMAGE_REPOSITORY_NAME"
 CACHE_REPOSITORY_PATH="ghcr.io/$IMAGE_REPOSITORY_PATH"
 
 {
-    echo "FLUTTER_VERSION=$(jq -r '.flutter.version' ${{ env.VERSION_MANIFEST }})"
-    echo "FASTLANE_VERSION=$(jq -r '.fastlane.version' ${{ env.VERSION_MANIFEST }})"
-    echo "ANDROID_PLATFORM_VERSIONS=$(jq -r '.android.platforms[].version' config/version.json)"
+    echo "FLUTTER_VERSION=$(jq -r '.flutter.version' "$VERSION_MANIFEST")"
+
+    echo "FASTLANE_VERSION=$(jq -r '.fastlane.version' "$VERSION_MANIFEST")"
+
+    echo "ANDROID_PLATFORM_VERSIONS=$(jq -r '.android.platforms[].version' "$VERSION_MANIFEST")"
+
     echo "IMAGE_REPOSITORY_PATH=$IMAGE_REPOSITORY_PATH"
+
     echo "CACHE_REPOSITORY_PATH=$CACHE_REPOSITORY_PATH"
-} >> "$GITHUB_ENV"
+} >>"$GITHUB_ENV"
