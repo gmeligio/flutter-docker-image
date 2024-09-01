a4bccacd2d4af7090fccd1e2de496ad617412c90
Eligio Mariño <22875166+gmeligio@users.noreply.github.com>
2024-09-01 11:58:47 -0400

    ci: use github integration for docker buildx cache (#218)

--- script/set_environment_variables.sh
+++ script/set_environment_variables.sh
@@ -11,6 +11,4 @@ CACHE_REPOSITORY_PATH="ghcr.io/$IMAGE_REPOSITORY_PATH"
     echo "ANDROID_PLATFORM_VERSIONS=$(jq -r '.android.platforms[].version' "$VERSION_MANIFEST" | tr '\n' ' ' | sed 's/ $//')"
 
     echo "IMAGE_REPOSITORY_PATH=$IMAGE_REPOSITORY_PATH"
-
-    echo "CACHE_REPOSITORY_PATH=$CACHE_REPOSITORY_PATH"
 } >>"$GITHUB_ENV"
