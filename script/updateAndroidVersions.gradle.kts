2e59fdfb0a1f5abddd2afea3fb4804084797eb4e
Eligio Mariño <22875166+gmeligio@users.noreply.github.com>
2025-02-15 17:16:05 +0100

    refactor: migrate Android version update script to Kotlin DSL and remove Groovy version (#288)
    * refactor: migrate Android version update script to Kotlin DSL and remove Groovy version
    
    * fix: correct file extension for Android version update script in workflows
    
    * docs: restore comment

---
+++ script/updateAndroidVersions.gradle.kts
@@ -0,0 +1,36 @@
+// Snippet to include at the end of android/app/build.gradle.kts
+
+tasks.register<DefaultTask>("updateAndroidVersions") {
+    doLast {
+        val jsonFile = File("../../config/version.json")     
+
+        // Parse existing JSON file
+        val resultJsonMap = groovy.json.JsonSlurper().parseText(jsonFile.readText()) as MutableMap<String, Any>
+
+        // Get unique platform versions
+        val platformVersions = listOf(
+            flutter.targetSdkVersion,
+            flutter.compileSdkVersion
+        ).distinct()
+
+        // Create new Android version data
+        val newJsonMap = mapOf(
+            "platforms" to platformVersions.map {
+                mapOf("version" to it)
+            },
+            "gradle" to mapOf("version" to gradle.gradleVersion)
+        )
+
+        // Merge new values into the existing JSON structure
+        (resultJsonMap["android"] as? MutableMap<String, Any>)?.putAll(newJsonMap)
+
+        // Format JSON with pretty printing
+        val jsonStr = groovy.json.JsonOutput.toJson(resultJsonMap)
+        val prettyStr = groovy.json.JsonOutput.prettyPrint(jsonStr)
+        
+        println(prettyStr)
+
+        // Write updated JSON back to the file
+        jsonFile.writeText("$prettyStr\n")
+    }
+}
