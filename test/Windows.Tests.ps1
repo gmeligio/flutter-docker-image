b41f5e5e90ea18f0133a6b62554b126abc8b1028
Eligio Mariño <22875166+gmeligio@users.noreply.github.com>
2025-05-11 17:35:19 +0200

    test: add version tests

--- test/Windows.Tests.ps1
+++ test/Windows.Tests.ps1
@@ -1,16 +1,26 @@
 Describe "Windows file structure tests" {
-
-    It "Should have C:\ProgramData directory" {
-        "C:\ProgramData" | Should -Exist
-    }
-
     It "Should have specific file content in config.txt" {
-        $content = Get-Content "C:\path\to\config.txt"
-        $content | Should -Match "expected value"
+        "$env:APPDATA\.dart-tool\dart-flutter-telemetry.config" | Should -FileContentMatchExactly "reporting=0"
     }
 
-    It "Should execute command and validate output" {
-        $result = & ipconfig
-        $result | Should -Match "IPv4 Address"
+    Context "VisualStudio components" {
+        BeforeAll {
+            $visualStudioPackages = (Get-ChildItem $env:ProgramData\Microsoft\VisualStudio\Packages).Name
+        }    
+
+        It "CMake version matches" {
+            $directoryName = $visualStudioPackages | Select-String -CaseSensitive Microsoft.VisualStudio.Component.VC.CMake.Project
+            $directoryName | Should -BeExactly "Microsoft.VisualStudio.Component.VC.CMake.Project,version=17.13.35710.127"
+        }
+
+        It "Windows11SDK version matches" {
+            $directoryName = $visualStudioPackages | Select-String -CaseSensitive Microsoft.VisualStudio.Component.Windows11SDK
+            $directoryName | Should -BeExactly "Microsoft.VisualStudio.Component.Windows11SDK.22621,version=17.13.35710.127"
+        }
+
+        It "VCTools version matches" {
+            $directoryName = $visualStudioPackages | Select-String -CaseSensitive Microsoft.VisualStudio.Workload.VCTools
+            $directoryName | Should -BeExactly "Microsoft.VisualStudio.Workload.VCTools,version=17.13.35710.127"
+        }
     }
 }
