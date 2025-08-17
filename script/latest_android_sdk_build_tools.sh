f2c342df1e42758dfc4fc929b6245ad029ce468a
Eligio Mariño <22875166+gmeligio@users.noreply.github.com>
2025-08-17 17:41:05 +0200

    build: Flutter 3.35.1 installs Android buildTools 35.0.0

--- script/latest_android_sdk_build_tools.sh
+++ script/latest_android_sdk_build_tools.sh
@@ -1,1 +1,4 @@
 sdkmanager --list | grep 'build-tools' | awk '{print $1}' | grep -oP 'build-tools;\d+\.\d+\.\d+$' | tail -1
+
+# Get 36.0.0 from `build-tools;36.0.0:build-tools`
+curl -s https://raw.githubusercontent.com/flutter/flutter/refs/tags/3.35.1/engine/src/flutter/tools/android_sdk/packages.txt | grep 'build-tools' | awk -F'[;:]' '{print $2}'
